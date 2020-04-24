967fd6bb4a986799320641390053f4a21622c23d
Daniel Fenjves <dfenjves@users.noreply.github.com>
2020-04-24 10:27:02 -0400

    Update stocks.py

--- stocks.py
+++ stocks.py
@@ -2,10 +2,9 @@ import pymongo
 from pymongo import MongoClient
 
 client = MongoClient("mongodb+srv://student:PqyqrY2aEC22B5SB@cluster0-ya1yr.mongodb.net/stock-prices?retryWrites=true")
-db = client['<database>'] #Make sure to replace this with the name of the database!
+db = client['<database>'] #Make sure to replace this with the name of the database ('stock-prices')!
 collection = db.prices
 
 # write your queries here
 stocks = list(collection.find({}))
 print(stocks)
-
