daf34733b149de1ebe61c05db87ad0343356d118
Manny Rodriguez <66538419+er-arcadio@users.noreply.github.com>
2023-05-12 10:13:06 -0400

    Update stocks.py - Manny 5.12.23

--- stocks.py
+++ stocks.py
@@ -5,8 +5,12 @@ client = MongoClient(
   "mongodb+srv://admin:Hlw7V8CtNJ0hXsgk@cluster0.jrxen8d.mongodb.net/?retryWrites=true&w=majority"
 )
 
-db = client['stock-prices'] # Note: "stock-prices" is the name of the Database 
-collection = db.prices      # Note: "prices" is the name of the collectionimport pymongo
+#Make sure to replace this with the name of the database, which you can find in this lab's readme!
+db = client['DATABASE-NAME'] 
+
+# Note: "prices" is the name of the collection where all the data is stored
+collection = db.prices       
+
 
 # write your queries here (#1)
 all_stocks = list(collection.find({}))
