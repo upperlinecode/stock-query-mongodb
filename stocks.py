bc733c933cbc441b0570705bd5fd46761606c1a2
Manny Rodriguez <66538419+er-arcadio@users.noreply.github.com>
2023-05-12 10:09:02 -0400

    Update stocks.py

--- stocks.py
+++ stocks.py
@@ -1,10 +1,13 @@
 import pymongo
 from pymongo import MongoClient
 
-client = MongoClient("mongodb+srv://student:PqyqrY2aEC22B5SB@cluster0-ya1yr.mongodb.net/stock-prices?retryWrites=true")
-db = client['database-name'] #Make sure to replace this with the name of the database, which you can find in this lab's readme!
-collection = db.prices
+client = MongoClient(
+  "mongodb+srv://admin:Hlw7V8CtNJ0hXsgk@cluster0.jrxen8d.mongodb.net/?retryWrites=true&w=majority"
+)
 
-# write your queries here
-stocks = list(collection.find({}))
-print(stocks)
+db = client['stock-prices'] # Note: "stock-prices" is the name of the Database 
+collection = db.prices      # Note: "prices" is the name of the collectionimport pymongo
+
+# write your queries here (#1)
+all_stocks = list(collection.find({}))
+print(all_stocks)
